3062a82d977a6300b88e14c89a12236ef43e8af9
Matthew Ramsey <36775051+pixelking20@users.noreply.github.com>
2020-07-20 14:35:53 -0400

    Tower Facing Complete

---
+++ Week2/Assets/Tower/TowerScripts/TowerFacing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerFacing : MonoBehaviour
+{
+    
+    RaycastHit RayHit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RayHit)){
+            transform.forward = RayHit.point - transform.position;
+        }
+    }
+}
