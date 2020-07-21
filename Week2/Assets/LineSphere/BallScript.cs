cee3b8217dcf66c0e8bc5848e7c81ad13e66d1ab
LordofHats <Logan.Crosby09@yahoo.com>
2020-07-20 20:03:40 -0400

    Sixlets

--- Week2/Assets/LineSphere/BallScript.cs
+++ Week2/Assets/LineSphere/BallScript.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class BallScript : MonoBehaviour
 {
     public int type = 0;
-    public int indexPos;
+    public int indexPos = -1;
     public Vector3 nextPosition;
     public List<Vector3> positionlist;
     public bool positionLock = false;
     public ZumaManager manager;
+    public Material[] mats;
 
     void Start()
     {
         manager = GameObject.FindGameObjectWithTag("GameController").GetComponent<ZumaManager>();
         type = Random.Range(0,manager.typeCount);
+        gameObject.GetComponent<Renderer>().material = mats[type];
         positionlist = GameObject.FindGameObjectWithTag("PathManager").GetComponent<PathManager>().pathPositions;
     }
 
     void Update()
     {
-        nextPosition = positionlist[manager.ballArray.Count - (indexPos+1)];
-        transform.position = Vector3.Lerp(transform.position, nextPosition, 0.2f);
+        if(indexPos > 0)
+        {
+            nextPosition = positionlist[manager.ballArray.Count - (indexPos+1)];
+            transform.position = Vector3.Lerp(transform.position, nextPosition, 0.2f);
+        }
     }
     void OnTriggerEnter(Collider collider)
     {
@@ -29,7 +34,7 @@ public class BallScript : MonoBehaviour
         {
             indexPos = collider.GetComponent<BallScript>().indexPos;
             manager.DestroyBallChainAtInsert(indexPos, this.gameObject, 0);
+            positionLock = true;
         }
-        positionLock = true;
     }
 }
